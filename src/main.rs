0fd8dca975cd945ed5e600317ad0a23862b34dce
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-10-09 20:26:29 +0200

    Add program arguments to parse a source file and output its abstract syntax tree. Add an example file Use `EXE <input> <output>`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use leben_parsable::{Parsable, ScopedStream};
+
+mod parsing;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 3 { panic!("Usage: EXE <input> <output>") }
+    
+    let input_path = std::path::Path::new(&args[1]);
+    let buffer = std::fs::read(input_path)?;
+    
+    let mut stream = ScopedStream::new(&buffer);
+    let parsed = parsing::SourceFile::parse(&mut stream).expect("failed to parse");
+    
+    let config = ron::ser::PrettyConfig::default();
+    let formatted = ron::ser::to_string_pretty(&parsed, config).unwrap();
+    
+    let output_path = std::path::Path::new(&args[2]);
+    std::fs::write(output_path, formatted)?;
+    Ok(())
 }
 
 #[cfg(test)]
