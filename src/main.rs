9e9d081bd6bbf3ef1f87d6682190b4da44ee35e2
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-10-09 11:24:28 +0200

    Add basic unit tests for `derive(Parsable)`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod derive_tests {
+    use super::*;
+    use leben_parsable::*;
+    use serde::{Deserialize, Serialize};
+
+    type Space = CharLiteral<b' '>;
+    type EqualSign = CharLiteral<b'='>;
+
+    #[derive(Clone, Debug, Default, PartialEq, Eq, Parsable, Deserialize, Serialize)]
+    struct VariableDeclaration {
+        #[literal = b"test"]
+        ident: (),
+        s1: Ignore<Option<Space>>,
+        eq: EqualSign,
+        s2: Ignore<Option<Space>>,
+        #[literal = b"wow"]
+        expr: (),
+        end: EndOfStream,
+    }
+
+    #[test]
+    fn test_derive() {
+        assert_eq!(
+            Some(VariableDeclaration::default()),
+            VariableDeclaration::parse(&mut ScopedStream::new(b"test =wow"))
+        );
+
+        let parsed = VariableDeclaration::parse(&mut ScopedStream::new(b"test =wow"));
+
+        println!("{}", ron::ser::to_string_pretty(&parsed.unwrap(), ron::ser::PrettyConfig::default()).unwrap());
+    }
+}
